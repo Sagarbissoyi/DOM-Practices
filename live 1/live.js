d97a94412338575abe744af5c2b69cde3840e5ce
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-02-06 23:29:37 +0530

    Add live.html and live.js with initial structure and JavaScript notes

---
+++ live 1/live.js
@@ -0,0 +1,74 @@
+//////////////////Live class notes//////////////
+
+//  document means complete html page
+
+// .querySelector() - returns the first element that matches a specified CSS selector(s) in the document.
+
+// .getElementById() - returns an Element object representing the element whose id property matches the specified string.
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////  END   ///////////////////////////////////////////
+
+
+document.querySelector()  // returns the first element that matches a specified CSS selector(s) in the document.
+document.querySelectorAll()  // returns all elements that match a specified CSS selector(s) in the document.
+  document.getElementById()  // returns an Element object representing the element whose id property matches the specified string.
+
+document.getElementsByClassName()  // returns a collection of all elements in the document with the specified class name, as a NodeList object.
+
+let firstLable = document.querySelector("#age-label label"); //#age-label wasr the id of the div/parent div and label was the tag name
+
+let firstlable1 = document.querySelectorAll("#age-label label")[1]; // this will select the second label tag from the div with id age-label.
+
+let form_js = document.querySelector("form[name='search']"); // this will select the form with name search           
+
+
+let form_js1 = document.getElementsByTagName("form[name='search']"); // this will select the form with name search but this will not work on getElementsByTagName because it returns a collection of all elements in the document with the specified tag name, as a NodeList object. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

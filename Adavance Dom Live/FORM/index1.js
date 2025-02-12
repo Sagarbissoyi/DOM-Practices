4f596781a71d961fca6dcbd6309b1557633afe02
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-02-12 21:03:47 +0530

    Refactor form validation logic and enhance user feedback with improved error/success messages

--- Adavance Dom Live/FORM/index1.js
+++ Adavance Dom Live/FORM/index1.js
@@ -1,3 +1,5 @@
+/// usage of form/////////
+
 const name_box= document.getElementById('name');
 const email_box= document.getElementById('email');
 const password_box=document.getElementById('password');
@@ -6,9 +8,11 @@ const submit_button=document.getElementById('submit');
 const output=document.getElementById('output');
 const message = document.querySelector('.message');
 
-// const form = document.querySelector('form');
+const form = document.querySelector('form');
 
-submit_button.addEventListener('click',validateForm)
+//submit_button.addEventListener('click',validateForm)
+
+form.addEventListener("submit",validateForm)/////////////////new features of form
 
 function validateForm(eventDeatils){
 
@@ -18,35 +22,50 @@ function validateForm(eventDeatils){
     let password= password_box.value;
     let confirm_password= confirm_password_box.value;
 
+
+    if(name.length < 5) {
+        message.innerHTML = "Name should be atleast 5 characters"
+        message.className = "error"
+        // name_box.style.border="2px solid red"
+        // name_box.style.outline="none"
+    
+        return
+    }
+
+
+
  if(name=="" || email=="" || password=="" || confirm_password==""){
-    //  alert('Please fill all the fields BUDDY');
+    
      message.innerHTML='Please fill all the fields BUDDY';
-    //  message.style.color='red';
+   
 
     message.className="error";
     return
  }
+
+
+
  if(password != confirm_password){
     // alert('Passwords do not match');
     message.innerHTML='Passwords do not match';
      message.className="error";
     return;
  }
-  /// email & password validation 
+  // email & password validation 
 
-    // if(!email.includes('@')){
-    //     // alert('Email is not valid');
-    //     message.innerHTML='Email is not valid';
-    //      message.className="error";
-    //     return;
-    // }
+    if(!email.includes('@')){
+        // alert('Email is not valid');
+        message.innerHTML='Email is not valid';
+         message.className="error";
+        return;
+    }
     if(password != confirm_password){
         // alert('Password should be atleast 8 characters');
         message.innerHTML='Password should be atleast 8 characters';
          message.className="error";
         return;
     }
-  message.innerHTML="Registration Success";
+  message.innerHTML='Registration Success';
     message.className="success";
 ////DISPLAY////
     output.innerHTML=`
@@ -58,7 +77,26 @@ function validateForm(eventDeatils){
     `
 
 }
+  
 
+
+// name_box.addEventListener("change",detectError)////its only work when its focouse out
+
+
+name_box.addEventListener("input",detectError)//its decatet first 
+
+
+function detectError(){
+    let value = name_box.value;
+    if(value.length < 5 ){
+        name_box.style.border ="2px solid red"
+        name_box.style.outline="none"
+    }
+    else {
+        name_box.style.border="2px solid green";
+        name_box.style.outline="none";
+    }
+}
 
 
 

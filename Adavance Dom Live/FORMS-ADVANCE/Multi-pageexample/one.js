1f143408a38d7518971c5dde1e5002187a880383
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-02-16 00:17:29 +0530

    Add multi-page HTML structure and JavaScript for heading selection

---
+++ Adavance Dom Live/FORMS-ADVANCE/Multi-pageexample/one.js
@@ -0,0 +1,15 @@
+
+
+let heading1FromA=document.querySelectorAll('h1');
+
+
+
+console.log(heading1FromA)
+
+console.log(heading1FromA.innerText);
+
+let heading2FromB=document.querySelectorAll('h2');
+
+
+console.log(heading2FromB)
+console.log(heading2FromB.innerText); 

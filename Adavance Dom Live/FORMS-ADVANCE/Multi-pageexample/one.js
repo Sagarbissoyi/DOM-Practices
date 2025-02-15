5a2003676fb79e8c66968e81a478a294184f6563
sagarbissoyi <sagarbissoyi10@gmail.com>
2025-02-16 00:34:57 +0530

    Add additional information and inline script to display developer details

--- Adavance Dom Live/FORMS-ADVANCE/Multi-pageexample/one.js
+++ Adavance Dom Live/FORMS-ADVANCE/Multi-pageexample/one.js
@@ -1,15 +1,17 @@
 
 
-let heading1FromA=document.querySelectorAll('h1');
+// let heading1FromA=document.querySelectorAll('h1');
 
 
 
-console.log(heading1FromA)
+// console.log(heading1FromA)
 
-console.log(heading1FromA.innerText);
+// console.log(heading1FromA.innerText);
 
-let heading2FromB=document.querySelectorAll('h2');
+// let heading2FromB=document.querySelectorAll('h2');
 
 
-console.log(heading2FromB)
-console.log(heading2FromB.innerText); 
+// console.log(heading2FromB)
+// console.log(heading2FromB.innerText); 
+
+//its very complicated to write 
